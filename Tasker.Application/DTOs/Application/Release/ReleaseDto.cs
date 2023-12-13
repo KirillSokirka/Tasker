3d72333c91889d378e61581a0fc83d1969ad8c39
Ivan Vlasiuk <vlasiukk.i.v@gmail.com>
2023-12-13 16:39:26 +0200

    releaseDto added

---
+++ Tasker.Application/DTOs/Application/Release/ReleaseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Tasker.Application.DTOs.Application.Release
+{
+    public class ReleaseDto
+    {
+        public string? Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsReleased { get; set; } = false;
+
+        public DateTime CreationDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public string? ProjectId { get; set; }
+
+        public List<ReleaseTaskDto> Tasks { get; set; }
+    }
+}
